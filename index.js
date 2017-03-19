bcfd7db6a99488b3af47ffddabee4c2d8d643350
Ghaith Ali <ghaithammar2008@gmail.com>
2017-03-19 18:00:47 +0100

    added some weather cases. rainy and clear sky with pictures.

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-
-
-//my api from the openweathermap.org, including API key.
-//http://api.openweathermap.org/data/2.5/weather?q=aarhus,uk&appid=a6962f458611362a2ba095476afa36fc
+//using the API form openweathermap.org.
 
 $(document).ready(function(){
+    var elms = {
+        backgroundImg : $(".backgroundImgContainer"),
+        title: $("h1.title"),
+        city: $("p.city"),
+        currentWeather: $("p.currentWeather")
+    };
+
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(function(position) {
             console.log(position.coords.latitude, position.coords.longitude);
+            $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+position.coords.latitude+'&lon='+position.coords.longitude+'&appid=a6962f458611362a2ba095476afa36fc', function(json){
+                if(json){
+                    elms.city.text("Current City: "+json.name);
+                    elms.currentWeather.text("Current Weather: " + json.weather[0].main);
+                    console.log(json);
+                    console.log(json.weather);
+                    switch (json.weather[0].main){
+                        case "Rain":
+                            console.log("rainy");
+                            elms.backgroundImg.addClass("rain");
+                            break;
+                        case "Clear":
+                            console.log("Clear");
+                            elms.backgroundImg.addClass("clear");
+                            break;
+                    }
+                }
+            });
         });
     } else {
-
+        elms.title.text("Location is not available");
     }
 
-    $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=aarhus,uk&appid=a6962f458611362a2ba095476afa36fc', function(json){
-        if(json){
-            console.log(json);
-        }
-    });
 
 });
